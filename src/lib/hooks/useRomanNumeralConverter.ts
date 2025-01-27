1594290af55204e5936b556f5c3e9320d28f4a79
quagswagEthanZhao <yixinzhaousu@gmail.com>
2025-01-26 22:52:41 -0700

    Add readme for docker and swap to api call

--- src/lib/hooks/useRomanNumeralConverter.ts
+++ src/lib/hooks/useRomanNumeralConverter.ts
@@ -6,35 +6,21 @@ const useRomanNumeralConverter = () => {
   const [error, setError] = useState<string>('');
   const [loading, setLoading] = useState<boolean>(false);
 
-  const mockFetch = (
-    input: number
-  ): Promise<{ input: string; output: string }> => {
-    return new Promise((resolve, reject) => {
-      const ramdomSuccess = Math.random() > 0.1;
-
-      setTimeout(() => {
-        if (ramdomSuccess) {
-          resolve({ input: input.toString(), output: 'IV' });
-        } else {
-          reject(new Error('Failed'));
-        }
-      }, 3000);
-    });
-  };
-
   const fetchRomanNumeral = async (input: number): Promise<void> => {
     setError('');
     setResult('');
     setLoading(true);
 
     try {
-      //const res = await fetch(`http://localhost:8080/romannumeral?query=${input}`);
-      const data = await mockFetch(input);
-      // if (!res.ok){
-      //     const errorText = await res.text();
-      //     throw new Error(errorText || "Failed to fetch conversion");
-      // }
-      // const data : RomanNumeralResponse = await res.json();
+      const res = await fetch(
+        `http://localhost:8080/romannumeral?number=${input}`
+      );
+
+      if (!res.ok) {
+        const errorText = await res.text();
+        throw new Error(errorText || 'Failed to fetch conversion');
+      }
+      const data: RomanNumeralResponse = await res.json();
       setResult(data.output);
     } catch (err: any) {
       setError(err?.message || 'An unknow error occurred');
